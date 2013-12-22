96be47b52d581cd4ae48fd7d1ed4da05ff3eef02
rcreniaux <romain.creniaux@gmail.com>
2013-12-22 23:15:05 +0100

    search marche mieux mais pas top

--- test.jsf2.web/src/main/java/org/rcx/test/jsf2/web/Search.java
+++ test.jsf2.web/src/main/java/org/rcx/test/jsf2/web/Search.java
@@ -1,18 +1,10 @@
 package org.rcx.test.jsf2.web;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.primefaces.event.SelectEvent;
 import org.primefaces.model.tagcloud.TagCloudItem;
-import org.rcx.test.jsf2.metier.service.IMetier;
-import org.rcx.test.jsf2.persistence.entity.FavTrend;
 import org.rcx.test.jsf2.web.pfbean.TagCloudBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import utils.MyTagCloudItem;
 
@@ -20,44 +12,35 @@ public class Search implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private String searchValue;
+	private String searchText;
+	
+	private String searchValueDisplay;
 
 	private TagCloudBean tagCloudBean;
 
-	private IMetier metier;
-
-	public String getSearchValue() {
-		return searchValue;
+	public String getSearchText() {
+		System.out.println("getSearchText : " + searchText);
+		return searchText;
 	}
 
-	@PostConstruct
-	private void beforeFirstRender() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config-metier-dao.xml");
-		metier = (IMetier) ctx.getBean("metierREST");
-		List<FavTrend> favTrends = new ArrayList<FavTrend>();
-		
-		for (FavTrend favTrend : favTrends) {
-			tagCloudBean.getModel().addTag(new MyTagCloudItem(favTrend.getValue(), 3));
-		}
-		
-	}
-
-	public void setSearchValue(String searchValue) {
-		this.searchValue = searchValue;
+	public void setSearchText(String searchText) {
+		System.out.println("setSearchText : " + searchText);
+		this.searchText = searchText;
 	}
 
 	public void onSearch() {
+		setSearchValueDisplay(searchText);
 		incrementTagForSearch();
 	}
 
 	private void incrementTagForSearch() {
-		if ("".equals(searchValue)) {
+		if ("".equals(searchText)) {
 			return;
 		}
 
 		TagCloudItem searchValueTagCloudItem = null;
 		for (TagCloudItem tagCloudItem : tagCloudBean.getModel().getTags()) {
-			if (tagCloudItem.getLabel().equals(searchValue)) {
+			if (tagCloudItem.getLabel().equals(searchText)) {
 				searchValueTagCloudItem = tagCloudItem;
 				if (searchValueTagCloudItem.getStrength() == 5) {
 					return;
@@ -72,12 +55,13 @@ public class Search implements Serializable {
 			tagCloudBean.getModel().removeTag(searchValueTagCloudItem);
 		}
 		countTag++;
-		tagCloudBean.getModel().addTag(new MyTagCloudItem(searchValue, countTag));
+		tagCloudBean.getModel().addTag(new MyTagCloudItem(searchText, countTag));
 	}
 
 	public void onSelectTag(SelectEvent event) {
 		TagCloudItem item = (TagCloudItem) event.getObject();
-		searchValue = item.getLabel();
+		searchText = item.getLabel();
+		searchValueDisplay = item.getLabel();
 	}
 
 	public TagCloudBean getTagCloudBean() {
@@ -86,6 +70,14 @@ public class Search implements Serializable {
 
 	public void setTagCloudBean(TagCloudBean tagCloudBean) {
 		this.tagCloudBean = tagCloudBean;
+	}
+
+	public String getSearchValueDisplay() {
+		return searchValueDisplay;
+	}
+
+	public void setSearchValueDisplay(String searchValueDisplay) {
+		this.searchValueDisplay = searchValueDisplay;
 	}
 
 }
